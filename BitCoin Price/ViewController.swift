a4d7ab10a0283c39bff27becfef5a0160c353b4b
Caio <caiovr13@gmail.com>
2021-04-16 20:00:16 -0300

    Requisição em API usando Decodable para decodificar dados

--- BitCoin Price/ViewController.swift
+++ BitCoin Price/ViewController.swift
@@ -9,11 +9,72 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    let numberFormatter = NumberFormatter()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        if let url = URL(string: "https://blockchain.info/ticker") {
+            let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+                if error == nil {
+                    print("Sucess")
+                    
+                    do {
+                        let dataDecoded = try JSONDecoder().decode(Currency.self, from: data!)
+                        if let buyPrice = self.formatNumberToBRLString(number: dataDecoded.currencyDetails.buy) {
+                            print(buyPrice)
+                        }
+                        
+                    } catch { }
+                } else {
+                    print("Error")
+                }
+            }
+            task.resume()
+        }
     }
-
-
+    
+    func formatNumberToBRLString(number: Double) -> String? {
+        numberFormatter.decimalSeparator = "."
+        numberFormatter.numberStyle = .currency
+        numberFormatter.locale = Locale(identifier: "pt_BR")
+        if let price = self.numberFormatter.string(from: NSNumber(value: number)) {
+            return price
+        }
+        return nil
+    }
 }
 
+struct Currency: Decodable {
+    var currencyDetails: CurrencyDetails
+
+    init (currencyDetails: CurrencyDetails){
+        self.currencyDetails = currencyDetails
+    }
+    
+    enum CodingKeys: String, CodingKey {
+       case currencyDetails = "BRL"
+    }
+}
+
+struct CurrencyDetails: Decodable {
+    var fifthteenMin: Double
+    var last: Double
+    var buy: Double
+    var sell: Double
+    var symbol: String
+    
+    init(fifthteenMin: Double, last: Double, buy: Double, sell: Double, symbol: String) {
+        self.fifthteenMin = fifthteenMin
+        self.last = last
+        self.buy = buy
+        self.sell = sell
+        self.symbol = symbol
+    }
+    
+    enum CodingKeys: String, CodingKey {
+       case fifthteenMin = "15m", last, buy, sell, symbol
+    }
+    
+}
